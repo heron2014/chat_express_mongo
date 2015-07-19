d1b88dd5ec96bef8e9b7f0115f5ced96c41ba93e
Anita <a.nita@hotmail.co.uk>
2015-07-19 17:54:04 +0100

    unique usernames works

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http),
+nicknames = []; // list of our users connected to the chat
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
+  socket.on('new user', function(data, callback) {
+    if(nicknames.indexOf(data) !== -1) { //we are checking if the nickname already exist in our array
+      callback(false); // we are sending just value false to the client if nickname already exist
+    } else {
+      callback(true);
+      socket.nickname = data; //we are creating nickname property on socket object which will hold new nickname 
+      nicknames.push(socket.nickname); //we push that new nickname to our array
+      io.emit('usernames', nicknames); // we are sending nicknames back to client
+    }
+  });
+
   socket.on('send message', function(data) { // receive message from client
     io.emit('new message', data); //send it back to users
   });
