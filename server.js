39d0caeadfa32273d9591ade5dd756b012b1f265
Anita <a.nita@hotmail.co.uk>
2015-07-19 18:20:05 +0100

    updates correct list of users connected to chat and displays them

--- server.js
+++ server.js
@@ -16,13 +16,29 @@ io.on('connection', function(socket){
       callback(true);
       socket.nickname = data; //we are creating nickname property on socket object which will hold new nickname 
       nicknames.push(socket.nickname); //we push that new nickname to our array
-      io.emit('usernames', nicknames); // we are sending nicknames back to client
+      updateNicknames(); // we are sending nicknames back to client
     }
   });
+
+  function updateNicknames() {
+    io.emit('usernames', nicknames);
+  }
 
   socket.on('send message', function(data) { // receive message from client
     io.emit('new message', data); //send it back to users
   });
+
+  //disconnect users when they are not in chat - dont show usernames nicknames when they are not connected to chat
+  socket.on('disconnect', function(data) {
+    //check if users have set names
+    if (!socket.nickname) return;
+
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1); //delete nickname from the array 
+
+    //sending back the correct list of connected users
+    updateNicknames();
+  });
+
 });
 
 
