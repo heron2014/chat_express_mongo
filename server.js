2dcdf5c2581c59cd33c779c5030a1946f456a596
Anita <a.nita@hotmail.co.uk>
2015-07-19 18:41:40 +0100

    updates server side with users object - is no longer arrat - first step to private messages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http),
-nicknames = []; // list of our users connected to the chat
+users = {}; // list of our users connected to the chat, users is now an object because we want to store socket reference to each nickname 
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -10,18 +10,18 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket){
 
   socket.on('new user', function(data, callback) {
-    if(nicknames.indexOf(data) !== -1) { //we are checking if the nickname already exist in our array
+    if(data in users) { //we are checking if the nickname already exist in our array
       callback(false); // we are sending just value false to the client if nickname already exist
     } else {
       callback(true);
       socket.nickname = data; //we are creating nickname property on socket object which will hold new nickname 
-      nicknames.push(socket.nickname); //we push that new nickname to our array
+      users[socket.nickname] = socket;
       updateNicknames(); // we are sending nicknames back to client
     }
   });
 
   function updateNicknames() {
-    io.emit('usernames', nicknames);
+    io.emit('usernames', Object.keys(users)); //we are sending only nicknames (array) , no socket 
   }
 
   socket.on('send message', function(data) { // receive message from client
@@ -33,7 +33,8 @@ io.on('connection', function(socket){
     //check if users have set names
     if (!socket.nickname) return;
 
-    nicknames.splice(nicknames.indexOf(socket.nickname), 1); //delete nickname from the array 
+    delete users[socket.nickname];
+    // nicknames.splice(nicknames.indexOf(socket.nickname), 1); delete nickname from the array 
 
     //sending back the correct list of connected users
     updateNicknames();
